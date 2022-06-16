acea605917fd5f526579fd9f089741215f4b3dfc
Alberto8809 <albertohondalii@gmail.com>
2022-06-16 14:52:41 -0400

    port

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || '8080';
 const PUBLISHABLE_KEY = "pk_test_51L8XfnFsR5opKmH1Q89hJ3W90avZuAHGtmbLUuPE5yyE2YmoelT3zTueeoVPlYSm4qscuBkBbkb1lVdjz7HEBltT00BI02dz5i";
 const SECRET_KEY = "sk_test_51L8XfnFsR5opKmH1TEztjIzkolx956A9xpPGNmbCMQVVqQOdZLLgg5YFbhUe4RqbVKNdePrkLMX0rkHba55QG2da003fEd60Aq";
 import Stripe from 'stripe';
 const stripe = Stripe(SECRET_KEY, {apiVersion : '2020-08-27'});
 
-app.listen(5000, function(){
-    console.log('listening on *:5000');
-  });
+app.set("port", port);
+app.listen(port, ()=>{
+    console.log(`App listening at http://localhost:${port}`);
+});
+
 app.post('/create-payment-intent', async (req, res)=>{
     try {
         const paymentIntent = await stripe.paymentIntents.create({
