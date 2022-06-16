40243ef0aca13e756a9d51cf39d2d8204e3024db
Alberto8809 <albertohondalii@gmail.com>
2022-06-16 11:38:25 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+const PUBLISHABLE_KEY = "pk_test_51L8XfnFsR5opKmH1Q89hJ3W90avZuAHGtmbLUuPE5yyE2YmoelT3zTueeoVPlYSm4qscuBkBbkb1lVdjz7HEBltT00BI02dz5i";
+const SECRET_KEY = "sk_test_51L8XfnFsR5opKmH1TEztjIzkolx956A9xpPGNmbCMQVVqQOdZLLgg5YFbhUe4RqbVKNdePrkLMX0rkHba55QG2da003fEd60Aq";
+import Stripe from 'stripe';
+const stripe = Stripe(SECRET_KEY, {apiVersion : '2020-08-27'});
+
+app.listen(port, ()=>{
+    console.log(`App listening at http://localhost:${port}`);
+});
+app.post('/create-payment-intent', async (req, res)=>{
+    try {
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount : 1099,
+            currency : "usd",
+            payment_method_types : ["card"],
+        });
+        const clientSecret = paymentIntent.client_secret;
+        console.log("clientSecret ", clientSecret);
+        res.json({
+            clientSecret : clientSecret
+        })
+    } catch (error) {
+        console.log("error ", error);
+        res.json({ error : error.message })
+    }
+})
+
+
+
